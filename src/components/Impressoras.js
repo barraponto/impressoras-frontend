53544d9ee87a23ff66bc05454e4771b4caf0abf0
Capi Etheriel <barraponto@gmail.com>
2019-06-15 09:18:17 -0300

    correção pequena

--- src/components/Impressoras.js
+++ src/components/Impressoras.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import ImpressoraStub from './ImpressoraStub';
+import ImpressoraStub from '.onChange/ImpressoraStub';
 
 const impressoras = [
   {
