a8d06cf5e13703607d451b20ffaf21f7f695f73c
Capi Etheriel <barraponto@gmail.com>
2019-12-07 10:07:31 -0300

    fixes solucao display

--- src/components/Solucao.js
+++ src/components/Solucao.js
@@ -23,7 +23,9 @@ export default ({match}) => {
   return (
     <div class="sugestao">
       <h1 className="h3 font-weight-normal text-center">Solução Sugerida</h1>
-      <div className="solucao" dangerouslySetInnerHTML={{__html: solucao.solucao }} />
+      <ol className="solucao">
+        {solucao.map(s => <li> { s.solucao }</li>)}
+      </ol>
       <button className="btn btn-primary btn-block mt-3" type="button">Problema Resolvido!</button>
       <button className="btn btn-secondary btn-block" type="button">Ainda não resolveu!</button>
       <small>Apresentando soluções para Impressora {marca} {modelo} número de série {serie} alocada em <em>{local}</em>.</small>
